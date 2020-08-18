e5c48222a5364944ea121881afd8885666eec673
Dave <dave@webdave.de>
2020-08-18 16:47:58 +0200

    Generate a Navigation Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopNavComponent],
   imports: [BrowserModule, AppRoutingModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent],
