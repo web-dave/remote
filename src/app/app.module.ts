b7858f24717e9dc9c31456744f4fc98a11421cae
Dave <dave@webdave.de>
2020-08-19 14:11:41 +0200

    Create an Info-Box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
 import { TopNavComponent } from './top-nav/top-nav.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
 
 @NgModule({
-  declarations: [AppComponent, TopNavComponent],
+  declarations: [AppComponent, TopNavComponent, InfoBoxComponent],
   imports: [BrowserModule, AppRoutingModule, BooksModule, AboutModule],
   providers: [],
   bootstrap: [AppComponent],
