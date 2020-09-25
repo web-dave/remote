59ff2eddcc53b98cd46dd23970f2c4b948804cbc
Dave <dave@webdave.de>
2020-09-25 08:51:35 +0200

    made it prettier

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -5,43 +5,43 @@ import { delay, share, shareReplay } from 'rxjs/operators';
 import { IBook } from './ibook';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-  url = 'http://localhost:4730/books/'
-  private books: IBook[] =  [
+  url = 'http://localhost:4730/books/';
+  private books: IBook[] = [
     {
-      title: "Design Patterns",
-      subtitle: "Elements of Reusable Object-Oriented Software",
-      isbn: "1"
+      title: 'Design Patterns',
+      subtitle: 'Elements of Reusable Object-Oriented Software',
+      isbn: '1',
     },
     {
-      title: "REST und HTTP",
-      subtitle: "Entwicklung und Integration nach dem Architekturstil des Web",
-      isbn: "2"
+      title: 'REST und HTTP',
+      subtitle: 'Entwicklung und Integration nach dem Architekturstil des Web',
+      isbn: '2',
     },
     {
-      title: "Eloquent JavaScript",
-      subtitle: "A Modern Introduction to Programming",
-      isbn:"3"
-    }
-  ]
+      title: 'Eloquent JavaScript',
+      subtitle: 'A Modern Introduction to Programming',
+      isbn: '3',
+    },
+  ];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getBooks(): Observable<IBook[]>{
-  return this.http.get<IBook[]>(this.url).pipe(shareReplay())
+  getBooks(): Observable<IBook[]> {
+    return this.http.get<IBook[]>(this.url).pipe(shareReplay());
   }
-  getBooks1(): Observable<IBook>{
-    return from(this.books)
+  getBooks1(): Observable<IBook> {
+    return from(this.books);
   }
 
   // getT(){
   //   return interval(1500)
   // }
 
-  getBooksSync(){
-    console.log('getBooks')
+  getBooksSync() {
+    console.log('getBooks');
     return this.books;
   }
 }
