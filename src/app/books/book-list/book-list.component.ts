ef9685a7187efaaf2c7837d0252f98c98940ca06
Dave <dave@webdave.de>
2020-09-25 09:05:05 +0200

    clean up

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -7,24 +7,20 @@ import { IBook } from '../ibook';
 @Component({
   selector: 'books-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit, OnDestroy {
   books: IBook[] = [];
   books$: Observable<IBook[]>;
-  subs = new Subscription()
-  constructor( public service: BookService) { }
+  subs = new Subscription();
+  constructor(public service: BookService) {}
 
   ngOnInit(): void {
-    this.books$ = this.service.getBooks();//.subscribe(data => this.books = data);
-    this.subs.add(this.service.getBooks().subscribe());
-    this.subs.add(this.service.getBooks1().subscribe(data => console.log(data)));
-    // this.service.getT().subscribe(t => console.log(t));
+    this.books$ = this.service.getBooks();
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     console.log('Bye Bye');
-    this.subs.unsubscribe()
+    this.subs.unsubscribe();
   }
-
 }
--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -9,39 +9,10 @@ import { IBook } from './ibook';
 })
 export class BookService {
   url = 'http://localhost:4730/books/';
-  private books: IBook[] = [
-    {
-      title: 'Design Patterns',
-      subtitle: 'Elements of Reusable Object-Oriented Software',
-      isbn: '1',
-    },
-    {
-      title: 'REST und HTTP',
-      subtitle: 'Entwicklung und Integration nach dem Architekturstil des Web',
-      isbn: '2',
-    },
-    {
-      title: 'Eloquent JavaScript',
-      subtitle: 'A Modern Introduction to Programming',
-      isbn: '3',
-    },
-  ];
 
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<IBook[]> {
     return this.http.get<IBook[]>(this.url).pipe(shareReplay());
-  }
-  getBooks1(): Observable<IBook> {
-    return from(this.books);
-  }
-
-  // getT(){
-  //   return interval(1500)
-  // }
-
-  getBooksSync() {
-    console.log('getBooks');
-    return this.books;
   }
 }
